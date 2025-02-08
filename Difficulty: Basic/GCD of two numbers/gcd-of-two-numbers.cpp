ca43ba535042edc64c82ca787624cd4628430822
Karitk Gupta <csai23031@glbitm.ac.in>
2025-02-08 07:39:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/GCD of two numbers/gcd-of-two-numbers.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+   int gcd(int a, int b)
+{
+    // Everything divides 0
+    if (a == 0)
+        return b;
+    if (b == 0)
+        return a;
+
+    // Base case
+    if (a == b)
+        return a;
+
+    // a is greater
+    if (a > b)
+        return gcd(a - b, b);
+    return gcd(a, b - a);
+}
+
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int a;
+        scanf("%d",&a);
+        
+        
+        int b;
+        scanf("%d",&b);
+        
+        Solution obj;
+        int res = obj.gcd(a, b);
+        
+        cout<<res<<endl;
+        
+    
+cout << "~" << "\n";
+}
+}
+
+// } Driver Code Ends
