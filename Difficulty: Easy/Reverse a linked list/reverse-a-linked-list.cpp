cefd66967427a91d3c850294c796aea082cb19f1
Karitk Gupta <csai23031@glbitm.ac.in>
2025-08-29 23:18:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reverse a linked list/reverse-a-linked-list.cpp
+++ Difficulty: Easy/Reverse a linked list/reverse-a-linked-list.cpp
@@ -13,18 +13,30 @@ class Node {
 
 class Solution {
   public:
-    Node* reverseList(struct Node* head) {
-        Node* prev = NULL;
-        Node* curr = head;
-
-        while (curr != NULL) {
-            Node* nextNode = curr->next; 
-            curr->next = prev;          
-            prev = curr;                
-            curr = nextNode;            
+    Node* reverseList(Node* head) {
+        vector<int> arr;
+        Node* temp=head;
+        while(temp!=NULL){
+            arr.push_back(temp->data);
+            temp=temp->next;
         }
-
-        return prev; 
         
+        int x=arr.size()-1;
+        int i=0;
+        while(x>i){
+            swap(arr[i],arr[x]);
+            i++;
+            x--;
+        }
+        
+        Node* dummy = new Node(arr[0]);
+        Node* current = dummy;
+    
+        for (int i = 1; i < arr.size(); i++) {
+            current->next = new Node(arr[i]);
+            current = current->next;
+        }
+    
+        return dummy;
     }
 };
