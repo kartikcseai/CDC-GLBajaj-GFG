d2c065a8e0126af4d4b7d45ba5e4cf3da2956a14
Karitk Gupta <csai23031@glbitm.ac.in>
2025-10-16 22:03:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/String Duplicates Removal/string-duplicates-removal.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    string removeDuplicates(string &s) {
+        int index = 0;
+        for (int i = 0; i < s.size(); i++) {
+            int j;
+            for (j = 0; j < i; j++) {
+                if (s[i] == s[j])
+                    break;  // duplicate found
+            }
+            if (j == i)  // no duplicate found
+                s[index++] = s[i];
+        }
+        s.resize(index);
+        return s;
+    }
+};
